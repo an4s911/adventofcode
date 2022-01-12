98753aeb37abd34abdce0d633b01676f29d2cce5
Anas Basheer <nasbas23@gmail.com>
2022-01-12 21:55:53 +0300

    Finished Day 10

--- python/10/10.py
+++ python/10/10.py
@@ -49,6 +49,53 @@ def part1():
 
 part1()
 
+# Part 2
+
+
+def part2():
+    points = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+
+    expected_chars = []
+    temp_lines = deepcopy(lines)
+    for line in lines:
+        stack = []
+        for i in range(len(line)):
+            element = line[i]
+            if element in closing_brackets:
+                stack.append(closing_brackets[element])
+            else:
+                expected = stack.pop()
+                if (expected != element):
+                    # expected_chars.append(expected)
+                    temp_lines.remove(line)
+
+        if line in temp_lines:
+            expected_chars.append(stack[::-1])
+        # print(stack)
+
+    # pprint(expected_chars)
+    scores = []
+
+    for closing in expected_chars:
+        score = 0
+        for elem in closing:
+            score *= 5
+            score += points[elem]
+        scores.append(score)
+
+    scores.sort()
+
+    # pprint(scores)
+    print(scores[len(scores)//2])
+
+
+part2()
+
 # def opens_and_closes(open_brac, close_brac):
 #     if brackets[open_brac] + brackets[close_brac] == 0:
 #         return True
